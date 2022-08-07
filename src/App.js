e0b1da0bb08b2c9cc3fe9b40f226b76edf2a0d51
fabian <fabifevi@gmail.com>
2022-08-07 22:20:43 +0200

    add svg creator, add mint functionality and zodiac sign system

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { Contract, providers } from 'ethers';
 import NFT from './abi/horoscope.json';
+import signs from './zodiacSigns.json';
 
 function App() {
 
+  const rynkebyId = "4";
   const [account, setAccount] = useState('');
   const [date, setDate] = useState("1992-08-31");
   const [nftBackground, setNftBackground] = useState("#8789C0");
+  const [zodiacSign, setZodiacSign] = useState(null);
+  const [zodiacIcon, setZodiacIcon] = useState(null);
+  const [NFTContract, setNFTContract] = useState(null);
+  const [networkId, setNetworkId] = useState(null);
+  const NFT_CONTRACT_ADDRESS = '0x463C43d1747cD7d8e1d86B0bB23e05e8397dfd47';
 
   const connectWallet = async () => {
     window.ethereum.request({ method: 'eth_requestAccounts' })
@@ -27,10 +34,16 @@ function App() {
     }
   }
 
+  const getNetworkId = async () => {
+    setNetworkId(await window.ethereum.networkVersion);
+  }
+
+  window.ethereum.on('networkChanged', getNetworkId);
   window.ethereum.on('accountsChanged', checkConnection);
 
   useEffect(() => {
     checkConnection();
+    getNetworkId();
   });
 
   function handleDateInput({ target }) {
@@ -39,6 +52,47 @@ function App() {
 
   function handleColorInput({ target }) {
     setNftBackground(target.value);
+  }
+
+  useEffect(() => {
+    calculateZodiacSign(date);
+  }, [date]);
+
+  function calculateZodiacSign(date) {
+    let dateObject = new Date(date);
+    let day = dateObject.getDate();
+    let month = dateObject.getMonth();
+
+    if (day >= signs[month].daySplit) {
+      setZodiacSign(signs[month].sign);
+      setZodiacIcon(signs[month].icon);
+    }else {
+      if (month === 0) {
+        setZodiacSign(signs[11].sign);
+        setZodiacIcon(signs[11].icon);
+      }else {
+        setZodiacSign(signs[month - 1].sign);
+        setZodiacIcon(signs[month - 1].icon);
+      }
+    }
+  }
+
+  useEffect(() => {
+    function initNFTContract() {
+      const provider = new providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setNFTContract(new Contract(NFT_CONTRACT_ADDRESS, NFT.abi, signer));
+    }
+    initNFTContract();
+  }, [account]);
+
+  const mintNft = async () => {
+    try {
+      let nftText = zodiacSign + " " + zodiacIcon;
+      await NFTContract.mintNFT(account, nftText, nftBackground);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   if (!window.ethereum) {
@@ -77,6 +131,36 @@ function App() {
           <label htmlFor="colorPicker">{" "}Select the background!</label>
         </div>
       </div>
+      {zodiacSign ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMinYMin meet"
+            viewBox="0 0 300 300"
+            width="400px"
+            height="400px"
+          >
+            <style>{`.base { fill: white; font-family: Arial; font-size: 24px;`}</style>
+            <rect width="100%" height="100%" fill={nftBackground} />
+            <text
+              x="50%"
+              y="50%"
+              className="base"
+              dominantBaseline="middle"
+              textAnchor="middle"
+            >
+              {zodiacSign + " " + zodiacIcon}
+            </text>
+          </svg>
+      ) : null}
+      {networkId === rynkebyId ? (
+        <button className='mintButton' onClick={mintNft}>
+        Mint NFT 🎉
+        </button>
+      ) : 
+        <button className='errorButton'>
+        Please switch to Rynkeby Testnet
+        </button>
+      }
     </div>
   );
 }
